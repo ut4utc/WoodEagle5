d92a5b09046a035ae497dcfeeb868db067e6c901
ut4utc <ut4utc@gmail.com>
2017-12-25 21:19:49 +0200

    fix attached files on css & php

--- layouts/attachment/item/textlink.php
+++ layouts/attachment/item/textlink.php
@@ -20,7 +20,7 @@ $config = KunenaConfig::getInstance();
 $attributesLink = $attachment->isImage() && $config->lightbox ? ' class="fancybox-button" rel="fancybox-button"' : '';
 ?>
 
-<a class="btn btn-small" rel="popover" data-placement="bottom" data-trigger="hover" target="_blank" data-content="Filesize: <?php echo number_format($attachment->size / 1024, 0, '', ',') . JText::_('COM_KUNENA_USER_ATTACHMENT_FILE_WEIGHT'); ?>
+<a class="popoverinfo" rel="popover" data-placement="bottom" data-trigger="hover" target="_blank" data-content="Filesize: <?php echo number_format($attachment->size / 1024, 0, '', ',') . JText::_('COM_KUNENA_USER_ATTACHMENT_FILE_WEIGHT'); ?>
 " data-original-title="<?php echo $attachment->getShortName(); ?>" href="<?php echo $attachment->getUrl(); ?>" title="<?php echo KunenaAttachmentHelper::shortenFileName($attachment->getFilename(), $config->attach_start, $config->attach_end); ?>">
-	<i class="icon icon-info"></i>
+	<i class="fa fa-info"></i>
 </a>
